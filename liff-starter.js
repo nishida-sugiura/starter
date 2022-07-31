da2550c73031824ef3d45db22bc264677bc9d9c0
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2022-08-01 00:30:39 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -130,7 +130,7 @@ function registerButtonHandlers() {
     // get profile call
     document.getElementById('getProfileButton').addEventListener('click', function() {
         liff.getProfile().then(function(profile) {
-            document.getElementById('userIdProfileField').textContent = profile.userId;
+            document.getElementById('userIdProfileField').textContent = profile.userId+profile.userId;
             document.getElementById('displayNameField').textContent = profile.displayName;
 
             
